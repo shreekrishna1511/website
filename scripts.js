e21e0d1beeb3cf0fe8f1d9066882c7f837f698ec
Shree Krishna Devkota <shreekrishna1514@gmail.com>
2024-05-13 19:25:11 +0545

    Add files via upload
    again

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+
+    var icon = document.getElementById("icon");
+
+	icon.onclick = function(){
+		document.body.classList.toggle("dark-theme");
+		if(document.body.classList.contains("dark-theme")){
+			icon.src = "sun.png";
+		}else{
+			icon.src = "moon.png";
+		}
+	}
+
+
+	var icon = document.getElementById("icon");
+
+	icon.onclick = function(){
+		document.body.classList.toggle("dark-theme");
+		if(document.body.classList.contains("dark-theme")){
+			icon.src = "sun.png";
+		}else{
+			icon.src = "moon.png";
+		}
+	}
